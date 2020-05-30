4d7629b346bbecd08e702602aa56e2c2767989ba
shreyaasridhar <shreyanju@gmail.com>
2020-05-30 05:32:38 -0700

    Added get

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,9 +2,21 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Ingredient, Dish
 
 from auth import AuthError, requires_auth
+
+ITEMS_PER_PAGE = 10
+
+
+def paginate(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+
+    return questions[start:end]
 
 
 def create_app(test_config=None):
@@ -19,8 +31,41 @@ def create_app(test_config=None):
 APP = create_app()
 
 
-# Error Handling
+@APP.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
+
+@APP.route('/ingredients')
+def view_ingredients():
+    selection = Ingredient.query.order_by(Ingredient.id).all()
+    current_ingredients = paginate(request, selection)
+    if len(current_ingredients) == 0:
+        abort(404)
+    return jsonify({
+        'success': True,
+        'ingredients': current_ingredients,
+        "total_ingredients": len(selection)
+    })
+
+
+@APP.route('/dishes')
+def view_dishes():
+    selection = Dish.query.order_by(Dselection=Dish.id).all()
+    current_dishes = paginate(request, selection)
+    if len(current_dishes) == 0:
+        abort(404)
+    return jsonify({
+        'success': True,
+        'dishes': current_dishes,
+        "total_dishes": len(selection)
+    })
+
+# Error Handling
 @APP.errorhandler(400)
 def bad_request(error):
     return jsonify({
