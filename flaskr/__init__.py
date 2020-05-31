aa6e8cc2d3f717a499ef0bfc2bc964676dcce5c4
shreyaasridhar <shreyanju@gmail.com>
2020-05-31 09:29:54 -0700

    fixed structure of create_app for test

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,179 +26,166 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    # drop_create_all()
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/ingredients')
+    @requires_auth("GET:ingredients")
+    def view_ingredients(jwt):
+        selection = Ingredient.query.order_by(Ingredient.id).all()
+        # current_ingredients = paginate(request, selection)
+        # if len(current_ingredients) == 0:
+        #     abort(404)
+        return jsonify({
+            'success': True,
+            'ingredients': [item.format() for item in selection],
+            "total_ingredients": len(selection)
+        })
+
+    @app.route('/dishes')
+    @requires_auth("GET:dishes")
+    def view_dishes(jwt):
+        selection = Dish.query.order_by(Dish.id).all()
+        # current_dishes = paginate(request, selection)
+        # if len(current_dishes) == 0:
+        #     abort(404)
+        return jsonify({
+            'success': True,
+            'dishes': [item.format() for item in selection],
+            "total_dishes": len(selection)
+        })
+
+    @app.route('/dishes', methods=['POST'])
+    @requires_auth("POST:dishes")
+    def add_new_dishes(jwt):
+        data = request.get_json()
+        dish = Dish(data['name'], data['image_link'],
+                    json.dumps(data['ingredients']))
+        dish.insert()
+        return jsonify({
+            'success': True
+        })
+
+    @app.route('/ingredients', methods=['POST'])
+    @requires_auth("POST:ingredients")
+    def add_new_ingredient(jwt):
+        data = request.get_json()
+        ingredient = Ingredient(
+            data['name'], data['image_link'], data['color'])
+        ingredient.insert()
+        return jsonify({
+            'success': True
+        })
+
+    @app.route('/dishes/<int:dish_id>', methods=['PATCH'])
+    @requires_auth("PATCH:dishes")
+    def update_dish(jwt, dish_id):
+        dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
+        if not dish:
+            abort(404)
+        data = request.get_json()
+        if 'name' in data:
+            setattr(dish, 'name', data['name'])
+        if 'image_link' in data:
+            setattr(dish, 'image_link', data['image_link'])
+        if 'ingredients' in data:
+            setattr(dish, 'ingredients', json.dumps(data['ingredients']))
+
+        dish.update()
+        return jsonify({
+            "success": True,
+            "dish": Dish.query.filter(Dish.id == dish_id).one_or_none().format()
+        })
+
+    @app.route('/ingredients/<int:ingredient_id>', methods=['PATCH'])
+    @requires_auth("PATCH:ingredients")
+    def update_ingredient(jwt, ingredient_id):
+        ingredient = Ingredient.query.filter(
+            Ingredient.id == ingredient_id).one_or_none()
+        if not ingredient:
+            abort(404)
+        data = request.get_json()
+        if 'name' in data:
+            setattr(ingredient, 'name', data['name'])
+        if 'image_link' in data:
+            setattr(ingredient, 'image_link', data['image_link'])
+        if 'color' in data:
+            setattr(ingredient, 'color', data['color'])
+
+        ingredient.update()
+        return jsonify({
+            "success": True,
+            "ingredient": Ingredient.query.filter(Ingredient.id == ingredient_id).one_or_none().format()
+        })
+
+    @app.route('/dishes/<int:dish_id>', methods=["DELETE"])
+    @requires_auth("DELETE:dishes")
+    def delete_dishes(jwt, dish_id):
+        dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
+        if not dish:
+            abort(404)
+        dish.delete()
+        return jsonify({
+            "success": True,
+            "delete": dish_id
+        })
+
+    @app.route('/ingredients/<int:ingredient_id>', methods=["DELETE"])
+    @requires_auth("DELETE:ingredients")
+    def delete_ingredient(jwt, ingredient_id):
+        ingredient = Ingredient.query.filter(
+            Ingredient.id == ingredient_id).one_or_none()
+        if not ingredient:
+            abort(404)
+        ingredient.delete()
+        return jsonify({
+            "success": True,
+            "delete": ingredient_id
+        })
+
+    # Error Handling
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def Notfound(error):
+        return jsonify({
+            "success": True,
+            "error": 404,
+            "description": "Resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(AuthError)
+    def autherror(error):
+        print(error.error)
+        return jsonify({
+            "success": True,
+            "error": error.status_code,
+            "description": error.error["description"]
+        }), error.status_code
+
     return app
 
 
-APP = create_app()
-# drop_create_all()
-
-
-@APP.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods',
-                         'GET,PUT,POST,DELETE,OPTIONS')
-    return response
-
-
-@APP.route('/ingredients')
-@requires_auth("GET:ingredients")
-def view_ingredients(jwt):
-    selection = Ingredient.query.order_by(Ingredient.id).all()
-    # current_ingredients = paginate(request, selection)
-    # if len(current_ingredients) == 0:
-    #     abort(404)
-    return jsonify({
-        'success': True,
-        'ingredients': [item.format() for item in selection],
-        "total_ingredients": len(selection)
-    })
-
-
-@APP.route('/dishes')
-@requires_auth("GET:dishes")
-def view_dishes(jwt):
-    selection = Dish.query.order_by(Dish.id).all()
-    # current_dishes = paginate(request, selection)
-    # if len(current_dishes) == 0:
-    #     abort(404)
-    return jsonify({
-        'success': True,
-        'dishes': [item.format() for item in selection],
-        "total_dishes": len(selection)
-    })
-
-
-@APP.route('/dishes', methods=['POST'])
-@requires_auth("POST:dishes")
-def add_new_dishes(jwt):
-    data = request.get_json()
-    dish = Dish(data['name'], data['image_link'],
-                json.dumps(data['ingredients']))
-    dish.insert()
-    return jsonify({
-        'success': True
-    })
-
-
-@APP.route('/ingredients', methods=['POST'])
-@requires_auth("POST:ingredients")
-def add_new_ingredient(jwt):
-    data = request.get_json()
-    ingredient = Ingredient(data['name'], data['image_link'], data['color'])
-    ingredient.insert()
-    return jsonify({
-        'success': True
-    })
-
-
-@APP.route('/dishes/<int:dish_id>', methods=['PATCH'])
-@requires_auth("PATCH:dishes")
-def update_dish(jwt, dish_id):
-    dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
-    if not dish:
-        abort(404)
-    data = request.get_json()
-    if 'name' in data:
-        setattr(dish, 'name', data['name'])
-    if 'image_link' in data:
-        setattr(dish, 'image_link', data['image_link'])
-    if 'ingredients' in data:
-        setattr(dish, 'ingredients', json.dumps(data['ingredients']))
-
-    dish.update()
-    return jsonify({
-        "success": True,
-        "dish": Dish.query.filter(Dish.id == dish_id).one_or_none().format()
-    })
-
-
-@APP.route('/ingredients/<int:ingredient_id>', methods=['PATCH'])
-@requires_auth("PATCH:ingredients")
-def update_ingredient(jwt, ingredient_id):
-    ingredient = Ingredient.query.filter(
-        Ingredient.id == ingredient_id).one_or_none()
-    if not ingredient:
-        abort(404)
-    data = request.get_json()
-    if 'name' in data:
-        setattr(ingredient, 'name', data['name'])
-    if 'image_link' in data:
-        setattr(ingredient, 'image_link', data['image_link'])
-    if 'color' in data:
-        setattr(ingredient, 'color', data['color'])
-
-    ingredient.update()
-    return jsonify({
-        "success": True,
-        "ingredient": Ingredient.query.filter(Ingredient.id == ingredient_id).one_or_none().format()
-    })
-
-
-@APP.route('/dishes/<int:dish_id>', methods=["DELETE"])
-@requires_auth("DELETE:dishes")
-def delete_dishes(jwt, dish_id):
-    dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
-    if not dish:
-        abort(404)
-    dish.delete()
-    return jsonify({
-        "success": True,
-        "delete": dish_id
-    })
-
-
-@APP.route('/ingredients/<int:ingredient_id>', methods=["DELETE"])
-@requires_auth("DELETE:ingredients")
-def delete_ingredient(jwt, ingredient_id):
-    ingredient = Ingredient.query.filter(
-        Ingredient.id == ingredient_id).one_or_none()
-    if not ingredient:
-        abort(404)
-    ingredient.delete()
-    return jsonify({
-        "success": True,
-        "delete": ingredient_id
-    })
-
-# Error Handling
-@APP.errorhandler(400)
-def bad_request(error):
-    return jsonify({
-        "success": False,
-        "error": 400,
-        "message": "bad request"
-    }), 400
-
-
-@APP.errorhandler(404)
-def Notfound(error):
-    return jsonify({
-        "success": True,
-        "error": 404,
-        "description": "Resource not found"
-    }), 404
-
-
-@APP.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
-
-
-@APP.errorhandler(AuthError)
-def autherror(error):
-    print(error.error)
-    return jsonify({
-        "success": True,
-        "error": error.status_code,
-        "description": error.error["description"]
-    }), error.status_code
-
-
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
