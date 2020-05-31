40e4a7b4e35c01b2da167b132e206f304503ed6d
shreyaasridhar <shreyanju@gmail.com>
2020-05-31 14:40:00 -0700

    Deployment settings

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
 
 database_name = "capstone"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
@@ -23,6 +24,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
+    migrate = Migrate(app, db)
     db.init_app(app)
     db.create_all()
 
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from flaskr import create_app
+
+APP = create_app()
+
+if __name__ == "__main__":
+    APP.run()
